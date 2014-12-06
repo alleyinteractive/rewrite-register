227bcc9f129e17114c2830b501f5a723445b6fa1
Matt Boynes <mboynes@alleyinteractive.com>
2014-12-05 22:18:39 -0500

    Move from calling this a queue to a register

--- class-rewrite-queue.php
+++ class-rewrite-queue.php
@@ -8,16 +8,71 @@ if ( ! class_exists( 'Rewrite_Queue' ) ) :
 
 class Rewrite_Queue {
 
+	/**
+	 * Hold the singleton instance.
+	 *
+	 * @access private
+	 *
+	 * @var object
+	 */
 	private static $instance;
 
-	protected $cached_rules = array();
+	/**
+	 * The option key in which to store the rewrite register.
+	 *
+	 * @access protected
+	 *
+	 * @var string
+	 */
+	protected $option = 'rewrite_register';
 
+	/**
+	 * The most recently generated register, as stored in the options.
+	 *
+	 * This is used to compare against the current register to see if anything
+	 * has changed.
+	 *
+	 * @access protected
+	 *
+	 * @var array
+	 */
+	protected $previous_register = array();
+
+	/**
+	 * The current register.
+	 *
+	 * As rule sets are registered, they're stored in this register. This is then
+	 * compared against the previous register, and becomes the foundation for
+	 * the rule queue should the rules need to be rebuilt.
+	 *
+	 * @access protected
+	 *
+	 * @var array
+	 */
+	protected $register = array();
+
+	/**
+	 * The rewrite rule queue used when building rewrites.
+	 *
+	 * The rule queue is an ordered set of rule set names. When building
+	 * rewrites, this queue is looped through and each name becomes part of a
+	 * fired action.
+	 *
+	 * @access protected
+	 *
+	 * @var array
+	 */
 	protected $queue = array();
 
 	private function __construct() {
 		/* Don't do anything, needs to be initialized via instance() method */
 	}
 
+	/**
+	 * Load the singleton instance.
+	 *
+	 * @return object
+	 */
 	public static function instance() {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new Rewrite_Queue;
@@ -26,17 +81,21 @@ class Rewrite_Queue {
 		return self::$instance;
 	}
 
+	/**
+	 * Setup the singleton. This is essentially a constructor and is only ever
+	 * run once.
+	 */
 	public function setup() {
-		$this->load_cached_queue();
-		add_action( 'wp_loaded', array( $this, 'do_enqueue_rewrites' ), 10 );
-		add_action( 'wp_loaded', array( $this, 'check_enqueued_rewrites' ), 15 );
+		$this->load_cached_register();
+		add_action( 'wp_loaded', array( $this, 'do_register_rewrites' ), 10 );
+		add_action( 'wp_loaded', array( $this, 'check_registered_rewrites' ), 15 );
 	}
 
 	/**
-	 * Enqueue a set of rewrite rules.
+	 * Register a set of rewrite rules.
 	 *
 	 * @param  string $name    A reference key for your rules. This must be
-	 *                         unique across all enqueued rewrites. When rules
+	 *                         unique across all registered rewrites. When rules
 	 *                         are being generated, the action
 	 *                         "build_rewrite_rules_{$name}"
 	 * @param  mixed $version  A version identifier. This will most often be an
@@ -46,33 +105,33 @@ class Rewrite_Queue {
 	 *                         changed.
 	 * @param  string $after   The placement for your rewrite rules. This can be
 	 *                         'top', 'bottom', or the $name of any other
-	 *                         enqueued rewrites.
+	 *                         registered rewrites.
 	 */
-	public function enqueue( $name, $version = null, $after = 'bottom' ) {
-		$this->queue[ $after ][ $name ] = $version;
+	public function register( $name, $version = null, $after = 'bottom' ) {
+		$this->register[ $after ][ $name ] = $version;
 	}
 
 	/**
 	 * Load the last generate queue from the options API. This is used to check
 	 * for new changes.
 	 */
-	protected function load_cached_queue() {
-		$this->cached_rules = get_option( 'rewrite_queue', array() );
+	protected function load_cached_register() {
+		$this->previous_register = get_option( $this->option, array() );
 	}
 
 	/**
-	 * Fire an action to enqueue rewrites. This offers a bit more granularity
+	 * Fire an action to register rewrites. This offers a bit more granularity
 	 * for rule order, since plugins can hook in to this action at any priority.
 	 *
 	 * If this were to ever become part or core, it could fire on its own (as
 	 * opposed to firing via wp_loaded).
 	 */
-	public function do_enqueue_rewrites() {
-		do_action( 'enqueue_rewrites' );
+	public function do_register_rewrites() {
+		do_action( 'register_rewrites' );
 	}
 
 	/**
-	 * Check the enqueued rules for any changes. Eventually we would identify
+	 * Check the registered rules for any changes. Eventually we would identify
 	 * what has changed, and selectively regenerate the rules which changed.
 	 * Changes that would be considered are:
 	 *
@@ -80,8 +139,8 @@ class Rewrite_Queue {
 	 * 2. Rule version numbers
 	 * 3. Rule order
 	 */
-	public function check_enqueued_rewrites() {
-		if ( $this->cached_rules !== $this->queue ) {
+	public function check_registered_rewrites() {
+		if ( $this->previous_register !== $this->register ) {
 			// There has been a change! For now, we'll just rebuild all rules.
 			$this->build_rules();
 		}
@@ -92,9 +151,9 @@ class Rewrite_Queue {
 	 * to only build the rules that changed, were added, or were removed.
 	 */
 	protected function build_rules() {
-		update_option( 'rewrite_queue', $this->queue );
+		update_option( $this->option, $this->register );
 
-		$this->reorder_queue();
+		$this->order_queue();
 
 		foreach ( $this->queue as $name ) {
 			do_action( 'build_rewrite_rules_' . $name );
@@ -107,7 +166,7 @@ class Rewrite_Queue {
 	/**
 	 * Put the queue in the order in which the rules should be built.
 	 */
-	protected function reorder_queue() {
+	protected function order_queue() {
 		// Set aside top rules
 		$ordered_rules = $this->dequeue_rules( 'top' );
 
@@ -115,7 +174,7 @@ class Rewrite_Queue {
 		$bottom_rules = $this->dequeue_rules( 'bottom' );
 
 		// Set aside the rest in the order they were received
-		$placements = array_keys( $this->queue );
+		$placements = array_keys( $this->register );
 		foreach ( $placements as $placement ) {
 			$ordered_rules = array_merge( $ordered_rules, $this->dequeue_rules( $placement ) );
 		}
@@ -123,7 +182,7 @@ class Rewrite_Queue {
 		// Add the bottom rules to the rest
 		$ordered_rules = array_merge( $ordered_rules, $bottom_rules );
 
-		// Ensure that all the rules are unique and add back to $this->queue.
+		// Ensure that all the rules are unique and store in the queue.
 		$this->queue = array_unique( $ordered_rules );
 	}
 
@@ -140,9 +199,9 @@ class Rewrite_Queue {
 	 */
 	protected function dequeue_rules( $placement ) {
 		$return = array();
-		if ( ! empty( $this->queue[ $placement ] ) ) {
-			$queue = $this->queue[ $placement ];
-			unset( $this->queue[ $placement ] );
+		if ( ! empty( $this->register[ $placement ] ) ) {
+			$queue = $this->register[ $placement ];
+			unset( $this->register[ $placement ] );
 
 			foreach ( $queue as $name => $version ) {
 				$return[] = $name;
--- rewrite-queue.php
+++ rewrite-queue.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
-	Plugin Name: Rewrite Queue
+	Plugin Name: Rewrite Register
 	Plugin URI: https://github.com/alleyinteractive/rewrite-queue
-	Description: A new way to think about rewrites rule manipulation.
+	Description: A new way to think about adding rewrite rules.
 	Version: 0.1
 	Author: Matthew Boynes
 	Author URI: http://www.alleyinteractive.com/
@@ -26,34 +26,34 @@
 require_once( dirname( __FILE__ ) . '/class-rewrite-queue.php' );
 
 /**
- * Enqueue a set of rewrite rules.
+ * Register a set of rewrite rules.
  *
- * @see  Rewrite_Queue::enqueue
+ * @see  Rewrite_Queue::register
  *
  * @param  string $name    A reference key for your rules.
  * @param  mixed $version  A version identifier.
  * @param  string $after   The placement for your rewrite rules.
  */
-function wp_enqueue_rewrites( $name, $version = null, $after = 'bottom' ) {
-	Rewrite_Queue()->enqueue( $name, $version, $after );
+function wp_register_rewrites( $name, $version = null, $after = 'bottom' ) {
+	Rewrite_Queue()->register( $name, $version, $after );
 }
 
 /**
- * Import the core rules to handle automated flushing. While we are "enqueueing"
+ * Import the core rules to handle automated flushing. While we are "registering"
  * these rules, they don't really play nice with this system (yet).
  */
 function temp_import_core_rules() {
 	global $wp_rewrite;
 
 	// Top
-	wp_enqueue_rewrites( 'core-top', md5( serialize( $wp_rewrite->extra_rules_top ) ), 'top' );
+	wp_register_rewrites( 'core-top', md5( serialize( $wp_rewrite->extra_rules_top ) ), 'top' );
 
 	// Permastructs
 	foreach ( $wp_rewrite->extra_permastructs as $name => $args ) {
-		wp_enqueue_rewrites( $name, md5( serialize( $args ) ) );
+		wp_register_rewrites( $name, md5( serialize( $args ) ) );
 	}
 
 	// Bottom
-	wp_enqueue_rewrites( 'core-bottom', md5( serialize( $wp_rewrite->extra_rules ) ) );
+	wp_register_rewrites( 'core-bottom', md5( serialize( $wp_rewrite->extra_rules ) ) );
 }
-add_action( 'enqueue_rewrites', 'temp_import_core_rules', 100 );
+add_action( 'register_rewrites', 'temp_import_core_rules', 100 );
